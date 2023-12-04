a4cbe45d391a0f73e959e12d875a3d47e87745b1
leo <leo.grandmorcel@ynov.com>
2023-12-04 09:46:55 +0100

    Essai Bonus

--- Bonus/script.asm
+++ Bonus/script.asm
@@ -1,34 +1,21 @@
-section .data
-    number db '0', 0
-    message db 'Number: ', 0
-
-section .text
-    global _start
-
+.global _start
+.data
+hi:.string "hi\n"
+.text
 _start:
-    mov ecx, 1 
-
-print_loop:
-    mov eax, 4
-    mov ebx, 1
-    mov edx, 8
-    mov esi, message
-    mov edi, edx
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov edx, 4
-    mov esi, number
-    mov edi, edx
-    int 0x80
-
-    inc ecx
-
-    cmp ecx, 10001
-    jle print_loop
-
+    mov $0,%r8
+    call hi_loop
+hi_loop:
+    mov $1,%rax
+    mov $1,%rdi
+    mov $hi,%rsi
+    mov $4,%rdx
+    syscall
+    add $1,%r8
+    cmp $3,%r8
+    jz exit
+    jmp hi_loop
 exit:
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov $60,%rax
+    mov $0,%rdi
+    syscall
